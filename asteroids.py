6719415b82f88e1d5acd8bda3b45966f5d2df8a0
dzejkop2 <91260006+dzejkop2@users.noreply.github.com>
2022-05-19 13:52:10 +0200

    nefunguje ani nebude fungovat

--- asteroids.py
+++ asteroids.py
@@ -1,40 +1,16 @@
 import math
 import random
 
+
 import pyglet
+import classes.spaceobject as SpaceObject
+from classes.spaceship import *
+from classes.constants import *
 from pyglet import gl
 from pyglet.window import key
 
 "---------Globalne konštanty a premenne----------"
 
-"Window constants"
-WIDTH = 1200
-HEIGHT = 800
-
-"Game constants"
-ACCELERATION = 120              #Zrýchlenie rakety
-ROTATION_SPEED = 0.05           #Rýchlosť otáčania rakety
-
-game_objects = []
-batch = pyglet.graphics.Batch() #ZOZNAM SPRITOV PRE ZJEDNODUŠENÉ VYKRESLENIE
-pressed_keyboards = set()       #MNOŽINA ZMAČKNUTÝCH KLÁVES
-
-delay_shooting = 0.4
-laserlifetime = 45
-laserspeed = 250
-
-shield_duration = 3
-
-"Score"
-score = 0
-
-lifes = 3
-max_score = 150
-
-pos_x = 0
-pos_y = 0
-rotation = 0
-"------------------- FUNKCIE __________________"
 
 """
 Vycentruj ukotvenie obrázka na stred
@@ -67,192 +43,13 @@ def draw_circle(x, y, radius):
 """
 Rodičovská trieda
 """
-class SpaceObject:
-    "Konštruktor"
-    def __init__(self, sprite, x, y, speed_x= 0, speed_y = 0):
-        self.x_speed = speed_x
-        self.y_speed = speed_y
-        self.rotation = 1.57  # radiany -> smeruje hore
 
-        self.sprite = pyglet.sprite.Sprite(sprite, batch=batch)
-        self.sprite.x = x
-        self.sprite.y = y
-        self.radius = (self.sprite.height + self.sprite.width) // 4
-
-    """
-    Výpočet vzdialenosti medzi dvoma objektami
-    Pytagorova veta
-    """
-    def distance(self, other):
-        x = abs(self.sprite.x - other.sprite.x)
-        y = abs(self.sprite.y - other.sprite.y)
-        return (x**2 + y**2) ** 0.5 #pytagorova veta
-
-    """
-    Kolizná metóda s loďou - nie je nutné defunovať, 
-    Definujeme až v odvodenej triede
-    """
-    def hit_by_spaceship(self, ship):
-        pass
-
-    """
-    Kolízna metóda s laserom - nie je nutné defynovať
-    Definujeme až v odvodenej triede
-    """
-    def hit_by_laser(self, laser):
-        pass
-
-    "Metoda ktora deletne objekt"
-    def delete(self, dt =0 ):
-        self.sprite.delete()
-        game_objects.remove(self)
-
-    """
-    Metóda pre kontrolu pozície či sa nachádzame na okraji
-    """
-    def checkBoundaries(self):
-        if self.sprite.x > WIDTH:
-            self.sprite.x = 0
-
-        if self.sprite.x < 0:
-            self.sprite.x = WIDTH
-
-        if self.sprite.y < 0:
-            self.sprite.y = HEIGHT
-
-        if self.sprite.y > HEIGHT:
-            self.sprite.y = 0
-
-    """
-    Metoda tick spoločná pre všetky podtriedy
-    """
-    def tick(self, dt):
-        "Posunutie vesmírnej lode na novú pozíciu"
-        self.sprite.x += dt * self.x_speed
-        self.sprite.y += dt * self.y_speed
-        self.sprite.rotation = 90 - math.degrees(self.rotation)
-
-        "Kontrola či sme prešli kraj"
-        self.checkBoundaries()
 
 """
 Trieda Spaceship
 Hlavný objekt hry, predstavuje hráča
 """
-class Spaceship(SpaceObject):
 
-    "Konśtruktor"
-    def __init__(self, sprite, x ,y):
-        super().__init__(sprite,x,y)
-        self.laser_ready = True
-        self.shield = False
-
-        #flame sprity
-        flame_sprite = pyglet.image.load("Assetss/PNG/Effects/fire05.png")
-        set_anchor_of_image_to_center(flame_sprite)
-        self.flame = pyglet.sprite.Sprite(flame_sprite,batch=batch)
-        self.flame.visible = False
-    
-    """
-    Metóda zodpovedná za vystrelenie laseru
-    """
-    def shoot(self):
-        img = pyglet.image.load("Assetss/PNG/Lasers/laserBlue04.png")
-        set_anchor_of_image_to_center(img)
-
-        laser_x = self.sprite.x + math.cos(self.rotation) * self.radius
-        laser_y = self.sprite.y + math.sin(self.rotation) * self.radius
-
-        laser = Laser(img,laser_x,laser_y)
-        laser.rotation = self.rotation
-
-        game_objects.append(laser)
-
-    def get_shield(self):
-        self.shield = True
-        img = pyglet.image.load('Assetss/PNG/Effects/shield1.png')
-        set_anchor_of_image_to_center(img)
-        shield = Shield(img, self.sprite.x, self.sprite.y)
-        
-        game_objects.append(shield)
-        pyglet.clock.schedule_once(self.shield_lose, shield_duration)
-
-    def shield_lose(self,dt):
-        self.shield = False
-        
-    def get_position(self):
-        global pos_x,pos_y,rotation
-        pos_x = self.sprite.x
-        pos_y = self.sprite.y
-        rotation = self.rotation
-
-    """
-    Každý frame sa vykoná táto metóda to znamená v našom prípade:
-    60 simkov * za sekundu
-    Mechanic of spaceship - rotation, movement, controls
-    """
-    def tick(self, dt):
-        super().tick(dt)
-
-        "Zrýchlenie po kliknutí klávesy W. Výpočet novej rýchlosti"
-        if 'W' in pressed_keyboards:
-            self.x_speed = self.x_speed + dt * ACCELERATION * math.cos(self.rotation)
-            self.y_speed = self.y_speed + dt * ACCELERATION * math.sin(self.rotation)
-
-            #flame pozicie a zobrazenie
-            self.flame.x = self.sprite.x - math.cos(self.rotation) * self.radius
-            self.flame.y = self.sprite.y - math.sin(self.rotation) * self.radius
-            self.flame.rotation = self.sprite.rotation
-            self.flame.visible = True
-        #ak nie je W tak flame neni vidno
-        else:
-            self.flame.visible = False
-        
-        "Spomalenie/spätný chod po kliknutí klávesy S"
-        if 'S' in pressed_keyboards:
-            self.x_speed = self.x_speed - dt * ACCELERATION * math.cos(self.rotation)
-            self.y_speed = self.y_speed - dt * ACCELERATION * math.sin(self.rotation)
-
-        "Otočenie doľava - A"
-        if 'A' in pressed_keyboards:
-            self.rotation += ROTATION_SPEED
-
-        "Otočenie doprava - D"
-        if 'D' in pressed_keyboards:
-            self.rotation -= ROTATION_SPEED
-
-        "Ručná brzda - SHIFT"
-        if 'SHIFT' in pressed_keyboards:
-            self.x_speed = 0
-            self.y_speed = 0
-
-        if "SPACE" in pressed_keyboards and self.laser_ready:
-            self.shoot()
-            self.laser_ready = False
-            pyglet.clock.schedule_once(self.reload, delay_shooting)
-        
-        if self.shield == True:
-            self.get_position()
-
-        "VYBERIE VŠETKY OSTATNE OBJEKTY OKREM SEBA SAMA"
-        for obj in [o for o in game_objects if o != self]:
-            # d = distance medzi objektami
-            d = self.distance(obj)
-            if d < self.radius + obj.radius:
-                obj.hit_by_spaceship(self)
-                break
-
-    "Metóda zodpovedná za reset pozície rakety"
-    def reset(self):
-        self.sprite.x = WIDTH // 2
-        self.sprite.y = HEIGHT // 2
-        self.rotation = 1.57  # radiany -> smeruje hore
-        self.x_speed = 0
-        self.y_speed = 0
-    
-    def reload(self,dt):
-        self.laser_ready = True
-        
 
 """
 Trieda Asteroid
